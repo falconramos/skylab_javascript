b092b40c28917c85d168a5d4087b6795ef2a54d4
falconramos <daniel@realidade3d.com.br>
2019-10-15 17:46:48 -0300

    Módulo 4 - passo 3 - importando o Axios

--- modulo4/main.js
+++ modulo4/main.js
@@ -1,10 +1,7 @@
-var xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'https://api.github.com/users/diego3g');
-xhr.send(null);
-
-xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText));
-    }
-}
+axios.get('https://api.github.com/users/diego3g')
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.warn(error);
+    });
